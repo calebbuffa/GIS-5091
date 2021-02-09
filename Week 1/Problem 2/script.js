f7ad21647112796c636fbca00b821184d6adb031
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2021-02-09 13:59:35 -0600

    Update script.js

--- Week 1/Problem 2/script.js
+++ Week 1/Problem 2/script.js
@@ -1,9 +1,13 @@
-var mymap = L.map('mapid').setView([37.7, -122.4], 10);
+var map = L.map('map').setView([37.7, -122.4], 10);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+  // load a tile layer
+var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(map);
 
  var ratIcon = L.icon({
     iconUrl:'https://cdn0.iconfinder.com/data/icons/isometric-city-basic-transport/480/car-police-front-01-512.png',
