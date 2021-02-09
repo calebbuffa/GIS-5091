928203525ef5ecebdebc428014f8ce4388e409d6
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2021-02-09 14:29:18 -0600

    Update script.js

--- Week 1/Problem 2/script.js
+++ Week 1/Problem 2/script.js
@@ -4,14 +4,3 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
-
-$.getJSON("https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/sf_crime.geojson",function(data){
-  var ratIcon = L.icon({
-    iconUrl: 'rat.png',
-    iconSize: [50,40]
-  }); 
-  L.geoJson(data  ,{
-    pointToLayer: function(feature,latlng){
-	  return L.marker(latlng,{icon: ratIcon});
-    }
-  });
