22de08ce9744e10f5e8f87d936bcb047e7add328
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2021-02-09 15:20:23 -0600

    Update script.js

--- Week 1/Problem 2/script.js
+++ Week 1/Problem 2/script.js
@@ -11,8 +11,7 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}
 $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/sf_crime.geojson",function(data){
   var ratIcon = L.icon({
     iconUrl: 'rat.png',
-    iconSize: [50,40]
-  }); 
+    iconSize: [50,40] 
   L.geoJson(data  ,{
     pointToLayer: function(feature,latlng){
 	  return L.marker(latlng,{icon: ratIcon});
