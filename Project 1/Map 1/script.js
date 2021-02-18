77d0844ab0b3ba8d0eb313263cdcac7972600550
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2021-02-18 17:07:23 -0600

    Update script.js

--- Project 1/Map 1/script.js
+++ Project 1/Map 1/script.js
@@ -13,9 +13,46 @@ var tstyle = {
   "fillOpacity": 1
 };
 
-L.geoJSON("terrorism.geojson", {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, tstyle);
-    }
-}).addTo(map);
+$.getJSON("https://raw.githubusercontent.com/calebbuffa/GIS-5091/main/Project%201/terrorism.geojson", function(response) {
+            console.log("response", response);
+            var terrorism = new L.GeoJSON(response, {
+              pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, tstyle);
+              }
+            }
+                                         );
+            terrorism.addTo(map); }); 
 
+var nstyle = {
+    radius: 1,
+    fillColor: "#4dff00",
+    color: "#4dff00",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: .1
+};
+
+$.getJSON("https://raw.githubusercontent.com/calebbuffa/GIS-5091/main/Project%201/fr_night_light.geojson", function(response) {
+            console.log("response", response);
+            var nlight = new L.GeoJSON(response, {
+              style: nstyle
+            }
+                                         );
+            nlight.addTo(map); }); 
+
+var rstyle = {
+    radius: .5,
+    fillColor: "#000000",
+    color: "#000000",
+    weight: .5,
+    opacity: .3,
+    fillOpacity: .3
+};
+
+$.getJSON("https://raw.githubusercontent.com/calebbuffa/GIS-5091/main/Project%201/FRA_roads.geojson", function(response) {
+            console.log("response", response);
+            var road = new L.GeoJSON(response, {
+              style: rstyle
+            }
+                                         );
+            road.addTo(map); }); 
