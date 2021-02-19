50d011f9e682c9dc840812486fe08eea78f9d8b1
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2021-02-18 19:04:24 -0600

    Update script.js

--- Project 1/Map 1/script.js
+++ Project 1/Map 1/script.js
@@ -1,8 +1,11 @@
 var map = L.map('map').setView([46.2276, 2.2137], 6);
 
-var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16
+var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
 }).addTo(map);
 
 var tstyle = {
