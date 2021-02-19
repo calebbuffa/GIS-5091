6a6d48c2fedb11f209fcb452e2c95f800c96de9e
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2021-02-18 18:15:48 -0600

    Update script.js

--- Project 1/Map 1/script.js
+++ Project 1/Map 1/script.js
@@ -1,8 +1,8 @@
 var map = L.map('map').setView([46.2276, 2.2137], 6);
 
-var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 var tstyle = {
