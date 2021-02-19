043e6c0ddb760168e83686dbd54cf45f48001be7
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2021-02-18 18:05:46 -0600

    Update script.js

--- Project 1/Map 1/script.js
+++ Project 1/Map 1/script.js
@@ -1,8 +1,8 @@
 var map = L.map('map').setView([46.2276, 2.2137], 6);
 
-var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16
+var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 var tstyle = {
