6896e6bd872099287b40ca280c6e7cd917ab64f2
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2021-02-19 07:35:59 -0600

    Update script.js

--- Project 1/Map 2/script.js
+++ Project 1/Map 2/script.js
@@ -5,57 +5,6 @@ var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/r
 	maxZoom: 16
 }).addTo(map);
 
-var tstyle = {
-    "color": "#ff0000",
-  "radius": 3,
-    "weight": 1,
-    "opacity": 1,
-  "fillOpacity": 1
-};
 
 var geojsonLayer = new L.GeoJSON.AJAX("terrorism.geojson");       
 geojsonLayer.addTo(map);
-
-$.getJSON("https://raw.githubusercontent.com/calebbuffa/GIS-5091/main/Project%201/terrorism.geojson", function(response) {
-            console.log("response", response);
-            var terrorism = new L.GeoJSON(response, {
-              pointToLayer: function (feature, latlng) {
-                return L.circleMarker(latlng, tstyle);
-              }
-            }
-                                         );
-            terrorism.addTo(map); }); 
-
-var nstyle = {
-    radius: 1,
-    fillColor: "#4dff00",
-    color: "#4dff00",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: .1
-};
-
-$.getJSON("https://raw.githubusercontent.com/calebbuffa/GIS-5091/main/Project%201/fr_night_light.geojson", function(response) {
-            console.log("response", response);
-            var nlight = new L.GeoJSON(response, {
-              style: nstyle
-            }
-                                         );
-            nlight.addTo(map); }); 
-
-var rstyle = {
-    radius: .5,
-    fillColor: "#000000",
-    color: "#000000",
-    weight: .5,
-    opacity: .3,
-    fillOpacity: .3
-};
-
-$.getJSON("https://raw.githubusercontent.com/calebbuffa/GIS-5091/main/Project%201/FRA_roads.geojson", function(response) {
-            console.log("response", response);
-            var roads = new L.GeoJSON(response, {
-              style: rstyle
-            }
-                                         );
-            roads.addTo(map); }); 
